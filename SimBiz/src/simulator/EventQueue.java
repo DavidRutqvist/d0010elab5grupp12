cb1b7df00b05e0fb506d266308d2ddceb3471442
DavidRutqvist <david.rutqvist@bnearit.se>
2014-03-05 09:09:50 +0100

    DEDS structure done

---
+++ SimBiz/src/simulator/EventQueue.java
@@ -0,0 +1,13 @@
+package simulator;
+
+import java.util.Iterator;
+
+public class EventQueue implements Iterable<Event> {
+
+	@Override
+	public Iterator<Event> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
