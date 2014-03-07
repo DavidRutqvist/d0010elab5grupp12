6fc0a924614693d2ef08cc657d96cbf60d33e8b4
Emil <Emil@192.168.0.195>
2014-03-07 17:45:51 +0100

    Added arguments to the constructors in CarWashStartEvent StopEvent.

--- SimBiz/src/carWashSimulator/MainSim.java
+++ SimBiz/src/carWashSimulator/MainSim.java
@@ -9,12 +9,15 @@ import simulator.StopEvent;
 public class MainSim {
 
 	public static void main(String[] args) {
+		final double STARTPRIORITY = 0;
+		final double STOPPRIORITY = 15;
+		
 		ArrayList<Event> events = new ArrayList<Event>();
 		CarWashState carState = new CarWashState();
 		CarWashView carView = new CarWashView(carState);
 		
-		events.add(new CarWashStartEvent());
-		events.add(new StopEvent());
+		events.add(new CarWashStartEvent(STARTPRIORITY));
+		events.add(new StopEvent(STOPPRIORITY));
 		
 		new Simulator(carState, events, carView);
 		
